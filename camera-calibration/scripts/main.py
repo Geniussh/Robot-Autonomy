756986f6068a3b2c02c4328d9a1d4129f8bcc828
Kelvin Shen <44150278+Geniussh@users.noreply.github.com>
2022-04-09 17:31:09 -0400

    Update main.py

--- camera-calibration/scripts/main.py
+++ camera-calibration/scripts/main.py
@@ -10,12 +10,76 @@ from utils import *
 AZURE_KINECT_INTRINSICS = 'calib/azure_kinect.intr'
 AZURE_KINECT_EXTRINSICS = 'calib/azure_kinect_overhead/azure_kinect_overhead_to_world.tf'
 
+cup_world = [0.5, 0, 0]
+
+rLower = np.array([160,59,20])
+rUpper = np.array([179,255,255])
+
+gLower = np.array([70,69,20])
+gUpper = np.array([83,255,255])
+
+bLower = np.array([102,7,28])
+bUpper = np.array([111,196,255])
+
+def define_borders(img):
+    border = []
+    
+    def drawBorder(action, x, y, flags, param):
+        if action == cv2.EVENT_LBUTTONDOWN:
+           param.append([x,y])
+        elif action == cv2.EVENT_LBUTTONUP:
+            param.append([x,y])
+        
+    cv2.namedWindow('image')
+    cv2.imshow('image', img)
+    cv2.setMouseCallback('image', drawBorder, border)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return border
+
+def find_drink(rgb_image, lower, upper):
+    hsv = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.dilate(mask, np.ones((5,5), np.uint8), iterations=1)
+    mask = cv2.erode(mask, np.ones((5,5), np.uint8), iterations=1)
+    mask = cv2.erode(mask, np.ones((5,5), np.uint8), iterations=1)
+    mask = cv2.dilate(mask, np.ones((5,5), np.uint8), iterations=1)
+    cnts, hie = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    maxc_i = np.argmax([cv2.contourArea(c) for c in cnts])
+    c = cnts[maxc_i]
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(rgb_image, (x-30,y-60), (x+w+30,y+h), (0,0,255), 2)
+
+    # Create a bounding box which only contains the upper half of the bottle
+    # HSV thresholding the black nozzle
+    nozzle_mask = np.zeros(rgb_image.shape[:2], np.uint8)
+    nozzle_mask[y-60:y+h, x-30:x+w+30] = 255
+    nozzle_mask = cv2.bitwise_and(rgb_image, rgb_image, mask=nozzle_mask)
+    nozzle_hsv = cv2.cvtColor(nozzle_mask, cv2.COLOR_BGR2HSV)
+    nozzle_mask = cv2.inRange(nozzle_hsv, np.array([0,1,0]), np.array([179,255,50]))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7))
+    nozzle_mask = cv2.morphologyEx(nozzle_mask, cv2.MORPH_CLOSE, kernel, iterations=5)
+    nozzle_mask = cv2.morphologyEx(nozzle_mask, cv2.MORPH_OPEN, kernel, iterations=5)
+    nozzle_cnts, _ = cv2.findContours(nozzle_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    nozzle_c = max(nozzle_cnts, key=cv2.contourArea)
+    (nozzle_x, nozzle_y), radius = cv2.minEnclosingCircle(nozzle_c)
+    center = [int(nozzle_x), int(nozzle_y)]
+    radius = int(radius)
+    cv2.circle(rgb_image, center, radius, (255,0,0), 2)
+
+    cv2.imshow('img', cv2.resize(rgb_image, (1024,768)))
+    cv2.waitKey(0)
+
+    return center
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--intrinsics_file_path', type=str, default=AZURE_KINECT_INTRINSICS)
     parser.add_argument('--extrinsics_file_path', type=str, default=AZURE_KINECT_EXTRINSICS) 
     args = parser.parse_args()
-
+    
     print('Starting robot')
     fa = FrankaArm()    
 
@@ -38,62 +102,77 @@ if __name__ == '__main__':
     cv2.imwrite('rgb.png', azure_kinect_rgb_image)
     cv2.imwrite('depth.png', azure_kinect_depth_image)
 
-    # object_image_position = np.array([800, 800])
+    # border = define_borders(azure_kinect_rgb_image)
+    # print(border)
+    border = [[366, 140], [1704, 922]]
+    mask = np.zeros(azure_kinect_rgb_image.shape[:2], np.uint8)
+    mask[border[0][1]:border[1][1], border[0][0]:border[1][0]] = 255
+    rgb_image = cv2.bitwise_and(azure_kinect_rgb_image, azure_kinect_rgb_image, mask=mask)
 
-    # def onMouse(event, x, y, flags, param):
-    #     if event == cv2.EVENT_LBUTTONDOWN:
-    #        print('x = %d, y = %d'%(x, y))
-    #        param[0] = x
-    #        param[1] = y
-        
-    cv2.namedWindow('image')
-    cv2.imshow('image', azure_kinect_rgb_image)
-    cv2.setMouseCallback('image', onMouse, object_image_position)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    center = find_drink(rgb_image, gLower, gUpper)
 
-    # object_z_height = 0.25
-    # intermediate_pose_z_height = 0.4
+    object_z_height = 0.2
+    intermediate_pose_z_height = 0.4
 
-    # object_center_point_in_world = get_object_center_point_in_world(object_image_position[0],
-    #                                                                 object_image_position[1],
-    #                                                                 azure_kinect_depth_image, azure_kinect_intrinsics,
-    #                                                                 azure_kinect_to_world_transform)
+    object_center_point_in_world = get_object_center_point_in_world(center[0],
+                                                                    center[1],
+                                                                    azure_kinect_depth_image, azure_kinect_intrinsics,
+                                                                    azure_kinect_to_world_transform)
 
-    # object_center_pose = fa.get_pose()
-    # object_center_pose.rotation = object_center_pose.rotation @ np.array([[0,1,0], [0,0,-1], [-1,0,0]])
-    # object_center_pose.translation = [object_center_point_in_world[0], object_center_point_in_world[1], object_z_height]
+    object_center_pose = fa.get_pose()
+    Ry = RigidTransform(rotation=RigidTransform.y_axis_rotation(np.deg2rad(90)), from_frame='franka_tool', to_frame='franka_tool')
+    object_center_pose = object_center_pose * Ry
+    object_center_pose.translation = [object_center_point_in_world[0], object_center_point_in_world[1], object_z_height]
 
-        
-    # # new_rotation = np.array([[np.cos(theta), -np.sin(theta), 0],
-    # #                       [-np.sin(theta), -np.cos(theta), 0],
-    # #                       [0, 0, -1]])
-    # # object_center_pose.rotation = new_rotation
-
-
-    # intermediate_robot_pose = object_center_pose.copy()
-    # intermediate_robot_pose.translation = [object_center_point_in_world[0], object_center_point_in_world[1], intermediate_pose_z_height]
-
-    # #Move to intermediate robot pose
-    # fa.goto_pose(intermediate_robot_pose)
-
-    # fa.goto_pose(object_center_pose, 5, force_thresholds=[10, 10, 10, 10, 10, 10])
-
-    # #Close Gripper
-    # fa.goto_gripper(0.045, grasp=True, force=10.0)
+    intermediate_robot_pose = object_center_pose.copy()
+    intermediate_robot_pose.translation = [object_center_point_in_world[0], object_center_point_in_world[1], intermediate_pose_z_height]
+    fa.goto_pose(intermediate_robot_pose)
     
-    # #Move to intermediate robot pose
+    #intermediate 1
+    # intermediate_robot_pose = fa.get_pose()
+    # intermediate_robot_pose.translation = [object_center_point_in_world[0], object_center_point_in_world[1], intermediate_pose_z_height]
+    # fa.goto_pose(intermediate_robot_pose)
+    
+    #intermediate 2
+    # Ry = RigidTransform(rotation=RigidTransform.y_axis_rotation(np.deg2rad(90)), from_frame='franka_tool', to_frame='franka_tool')
+    # intermediate_robot_pose = intermediate_robot_pose * Ry
     # fa.goto_pose(intermediate_robot_pose)
 
-    # fa.goto_pose(object_center_pose, 5, force_thresholds=[10, 10, 20, 10, 10, 10])
+    #Bottle
+    fa.goto_pose(object_center_pose, 5, force_thresholds=[10, 10, 10, 10, 10, 10])
 
-    # print('Opening Grippers')
-    # #Open Gripper
-    # fa.open_gripper()
+    fa.publish_joints()
 
-    # fa.goto_pose(intermediate_robot_pose)
+    #Close Gripper
+    fa.goto_gripper(0.045, grasp=True, force=10.0)
 
-    # #Reset Pose
-    # fa.reset_pose() 
-    # #Reset Joints
-    # fa.reset_joints()
+    #Move to intermediate robot pose
+    fa.goto_pose(intermediate_robot_pose)
+
+    #Move to cup intermediate
+    cup_intermediate_pose = intermediate_robot_pose.copy()
+    cup_intermediate_pose.translation[:2] = [cup_world[0], cup_world[1] + 0.1]
+    fa.goto_pose(cup_intermediate_pose)
+
+    #Move to cup
+    cup_pose = cup_intermediate_pose.copy()
+    cup_pose.translation[:2] = cup_world[:2]
+    R = RigidTransform(rotation=RigidTransform.z_axis_rotation(np.deg2rad(180)), from_frame='franka_tool', to_frame='franka_tool')
+    cup_pose = cup_pose * R
+    fa.goto_pose(cup_pose, 5, force_thresholds=[10, 10, 20, 10, 10, 10], buffer_time=5)
+
+    #Return
+    fa.goto_pose(cup_intermediate_pose)
+    fa.goto_pose(intermediate_robot_pose)
+    fa.goto_pose(object_center_pose, 5, force_thresholds=[10, 10, 20, 10, 10, 10])
+
+    print('Opening Grippers')
+    #Open Gripper
+    fa.open_gripper()
+
+    fa.goto_pose(intermediate_robot_pose)
+
+    #Reset Pose
+    fa.reset_pose() 
+    #Reset Joints
+    fa.reset_joints()
